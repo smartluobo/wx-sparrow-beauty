08f453c7387fe94e997a74c52a9ed201f9be0cb9
yeyuhan42 <1429202465@qq.com>
2019-11-24 13:24:42 +0800

    删除wx文件夹

--- wx/README.md
+++
@@ -1,2 +0,0 @@
-# wx-sparrow-beauty
-js
--- wx/template/courseList/courseList.js
+++
@@ -1,7 +0,0 @@
-const util = require('../../utils/util.js')
-Page({
-  data: {},
-  onLoad: function (options) {
-    console.info(this.data.picUrl)
-  }
-})
